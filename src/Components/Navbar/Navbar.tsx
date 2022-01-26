03939c4cb5515cdd9861a28b1807c461712b710c
uduma sonia <sohnyauduma@gmail.com>
2022-01-26 14:59:05 +0100

    navbar

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,10 +1,30 @@
 import React from "react";
 import "./Navbar.styles.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingBasket } from "@fortawesome/free-solid-svg-icons";
 
 export const Navbar: React.FC<{}> = () => {
   return (
-    <div>
-      <h1>NAVBAR</h1>
+    <div className="navbar-container">
+      <div className="container mx-auto px-5 full-height flex j-between items-center">
+        <h1 className="color-main fw-medium fs-md">Tap House</h1>
+
+        <div className="flex j-between fs-sm fw-semibold uppercase navbar-category">
+          <p>men</p>
+          <p>women</p>
+          <p>electronics</p>
+        </div>
+
+        <div className="width-8 flex j-between">
+          <button className="border-0 fs-sm-2 fw-semibold user-name">
+            Hi, Sonia
+          </button>
+
+          <button className="border-0">
+            <FontAwesomeIcon icon={faShoppingBasket} size="lg" />
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { HomeTopBar } from "../../Components/HomeTopBar/HomeTopBar";
+import { Navbar } from "../../Components/Navbar/Navbar";
 import "./Home.styles.css";
 
 export const Home: React.FC<{}> = () => {
   return (
     <div className="home-container">
       <HomeTopBar />
+      <Navbar />
     </div>
   );
 };
