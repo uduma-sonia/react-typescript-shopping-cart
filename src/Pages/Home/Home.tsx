315223813a5c37335954e6616506912f0aae7c82
uduma sonia <sohnyauduma@gmail.com>
2022-02-04 23:13:11 +0100

    fix: local storage error

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -63,6 +63,7 @@ export const Home: React.FC<{}> = () => {
     } else {
       setCartItems([]);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
